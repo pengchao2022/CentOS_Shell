a536097ebfb12f605c2eefada797c024d91f0f84
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-09-06 08:47:41 +0800

    Create personal_info.sh

---
+++ Input & Output/personal_info.sh
@@ -0,0 +1,19 @@
+
+
+#!/bin/bash
+
+read -p "Please enter your name here:" name
+
+echo "Your name is: $name"
+
+read -p "Please enter your gender here:" gender
+
+echo "Your gender is:$gender"
+
+read -p "Please enter your Age here:" age
+
+echo "Your Age is:$age"
+
+echo "You have finished the personal info input"
+
+echo "Thanks ! Bye-Bye!"
